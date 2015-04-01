ded17e12465e0176d3d9570b0f41dffb20b57307
mferrara <mf8yk@virginia.edu>
2015-04-01 14:40:03 -0600

    FIX & final search open script
    THIS SCRIPT AWAITS NEW CLASS ON BODY TAG - “FLYOUT-OPEN”

--- js/bhutan-search.js
+++ js/bhutan-search.js
@@ -3,9 +3,7 @@
 	Drupal.behaviors.bhutan_search_deafultOpen = {
 		attach: function (context, settings) {
 
-if($(".breadcrumb > li:eq(-1):contains('Bhutan Cultural Library')") && ($("body:contains('.flyout-open.page-places')"))) { $("#search-flyout").openMbExtruder().once(); }
-
-if($(".breadcrumb > li:eq(-1):contains('Bhutan')") && ($("body:contains('.flyout-open.page-subjects')"))) { $("#search-flyout").openMbExtruder().once(); }
+				if($(".flyout-open").length ) { $("#search-flyout").openMbExtruder().once(); }
 				
     }
   };	
