5bbd30ee153f12e4875d42256f5f65d7e0946fb0
mferrara <mf8yk@virginia.edu>
2015-04-01 13:33:58 -0600

    testing search open

--- js/bhutan-search.js
+++ js/bhutan-search.js
@@ -2,11 +2,11 @@
 
 	Drupal.behaviors.bhutan_search_deafultOpen = {
 		attach: function (context, settings) {
-			if(context == window.document) { 
+			// if(context == window.document) { 
 
-				if($(".breadcrumb li:eq(-1):contains('Bhutan Library')").length ) { $("#search-flyout").openMbExtruder().once(); }
+				if($(".breadcrumb").length ) { $("#search-flyout").openMbExtruder().once(); }
 				
-      }
+      // }
     }
   };	
   			
