429482cc0b8cd0079bcaebf2bdf7d72e6621fbef
mferrara <mf8yk@virginia.edu>
2015-04-01 10:38:54 -0600

    testing search opening function

--- js/bhutan-search.js
+++ js/bhutan-search.js
@@ -1,11 +1,10 @@
 (function ($) { // jQuery wrapper function
 
-	// Move the dom-id class so that the view block reloads all content and so does not duplicate filters, pager, etc.
 	Drupal.behaviors.bhutan_search_deafultOpen = {
 		attach: function (context, settings) {
 			if(context == window.document) { 
 
-				$("body.front #search-flyout").openMbExtruder().once();
+				if($("body.front #search-flyout").length ).openMbExtruder().once();
 				
       }
     }
