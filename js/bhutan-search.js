95cbdf61a370cb1481e3d509708c50ac0459293f
mferrara <mf8yk@virginia.edu>
2015-04-01 14:03:54 -0600

    final testing for search open

--- js/bhutan-search.js
+++ js/bhutan-search.js
@@ -2,13 +2,11 @@
 
 	Drupal.behaviors.bhutan_search_deafultOpen = {
 		attach: function (context, settings) {
-			// if(context == window.document) { 
 
 				if($(".breadcrumb > li:eq(-1):contains('Bhutan Cultural Library')") && ($(".breadcrumb > li:eq(1):contains('Home')"))) { $("#search-flyout").openMbExtruder().once(); }
 				
-				// if($(".breadcrumb > li:eq(-1):contains('Bhutan')").length ) { $("#search-flyout").openMbExtruder().once(); }
+				if($(".breadcrumb > li:eq(-1):contains('Bhutan')") && ($(".breadcrumb > li:eq(1):contains('Places')"))) { $("#search-flyout").openMbExtruder().once(); }
 				
-      // }
     }
   };	
   			
