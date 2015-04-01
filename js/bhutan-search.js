f8a311581a4ca97090eb84cccfebdcc135baa5f8
mferrara <mf8yk@virginia.edu>
2015-04-01 13:44:43 -0600

    search open testing

--- js/bhutan-search.js
+++ js/bhutan-search.js
@@ -4,7 +4,7 @@
 		attach: function (context, settings) {
 			// if(context == window.document) { 
 
-				if($(".breadcrumb li:eq(-1):contains('Bhutan Library')").length ) { $("#search-flyout").openMbExtruder().once(); }
+				if($(".breadcrumb > li:eq(-1):contains('Bhutan Cultural Library')").length ) { $("#search-flyout").openMbExtruder().once(); }
 				
       // }
     }
