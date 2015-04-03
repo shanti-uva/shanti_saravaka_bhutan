b34555a8fe0a3fc7916f4a54ddf45900b96b0f4b
ThanGrove <than@virginia.edu>
2015-04-03 08:04:04 -0400

    changing scroll bhutan to top code

--- js/bhutan-search.js
+++ js/bhutan-search.js
@@ -1,17 +1,18 @@
 (function ($) { // jQuery wrapper function
-
+	var bto;
 	Drupal.behaviors.bhutan_search_deafultOpen = {
 		attach: function (context, settings) {
 				if($(".flyout-open").length ) { 
 					$("#search-flyout").once('openflyout', function() {
 						$(this).openMbExtruder();
 						$('#kmaps-search .treeview a').click();
-						setTimeout(function() {
+						bto = setInterval(function() {
 							if($('#ajax-id-8260').length > 0) {
 								var topPos = document.getElementById('ajax-id-8260').offsetTop;
 								document.getElementById('tree').scrollTop = topPos;
+								window.clearInterval(bto);
 							} 
-						}, 2500);
+						}, 800);
 					});
 				}
 				
