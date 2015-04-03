e97f19f36aa4e928bb9dff646b57c09d7917276f
ThanGrove <than@virginia.edu>
2015-04-03 07:53:54 -0400

    adding scroll bhutan to top of tree

--- js/bhutan-search.js
+++ js/bhutan-search.js
@@ -2,12 +2,17 @@
 
 	Drupal.behaviors.bhutan_search_deafultOpen = {
 		attach: function (context, settings) {
-
 				if($(".flyout-open").length ) { 
 					$("#search-flyout").once('openflyout', function() {
 						$(this).openMbExtruder();
 						$('#kmaps-search .treeview a').click();
-					} );
+						setTimeout(function() {
+							if($('#ajax-id-8260').length > 0) {
+								var topPos = document.getElementById('ajax-id-8260').offsetTop;
+								document.getElementById('tree').scrollTop = topPos;
+							} 
+						}, 2500);
+					});
 				}
 				
     }
