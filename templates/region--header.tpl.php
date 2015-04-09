cd85f4fb7d8bbfe8ce34cfb65551d6369f05e204
mferrara <mf8yk@virginia.edu>
2015-04-09 11:31:08 -0500

    remove deprecated responsive menu dependents

--- templates/region--header.tpl.php
+++ templates/region--header.tpl.php
@@ -3,10 +3,7 @@
 	<div class="navbar navbar-default">
 		
     <nav class="navbar-buttons" role="navigation">
-      <span class="menu-icon shanti-searchtoggle"><a href="#"><span class="sr-only">Search</span><span class='icon shanticon-search'></span></a></span><!-- mobile < 400 : search -->
       <span class="menu-icon menu-toggle"><a href="#"><span class="sr-only">Main Menu</span><span class="icon shanticon-menu"></span></a></span><!-- desktop > 768 drilldown menu : main-menu -->
-      <span class="menu-icon menu-maintoggle"><a href="#"><span class="sr-only">Main Menu</span><span class="icon shanticon-menu"></span></a></span><!-- mobile < 768 : main-menu -->
-      <span class="menu-explore menu-exploretoggle"><a href="#"><span>Explore</span><span class="icon shanticon-directions"></span></a></span><!-- mobile < 768 : collections -->
     </nav>
 
     <h1 class="navbar-header<?php if(!$variables['shanti_site']) { print " default"; } ?>">
